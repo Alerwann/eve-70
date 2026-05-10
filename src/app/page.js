2922e213d723f7463d9f98c2c0ba158b6748282f
Alerwann <alerwann411@gmail.com>
2026-05-10 07:56:22 +0200

    feat: update home page content and ignore .prettierrc configuration

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-    <main>
-    </main>
+      <main>Bienvenue sur le site d'échanges d'annecdotes sur eve</main>
     </div>
   );
 }
